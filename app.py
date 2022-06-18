183f24fd5d9941e9f3f1792936a300cdd3810e5d
tusharkulkarni008 <tusharkulkarni95@gmail.com>
2022-06-18 12:17:42 +0530

    new file:   Housing/__init__.py 	new file:   Housing/component/__init__.py 	new file:   Housing/config/__init__.py 	new file:   Housing/entity/__init__.py 	new file:   Housing/exception/__init__.py 	new file:   Housing/logger/__init__.py 	new file:   Housing/pipeline/__init__.py 	modified:   app.py 	modified:   requirements.txt 	new file:   setup.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
+app=Flask(__name__)
 
-app = Flask(__name__)
 
-@app.route("/",methods = ['GET','POST'])
+@app.route("/",methods=['GET','POST'])
 def index():
-    return "Hello World"
-if __name__ == "__main__":
-    app.run(debug=True)
+    return "CI CD pipeline has been established."
+
+
+if __name__=="__main__":
+    app.run()
