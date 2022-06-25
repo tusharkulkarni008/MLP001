b983ed8bc9d59e43944aceb35fc2bb1c9290b9c9
tusharkulkarni008 <tusharkulkarni95@gmail.com>
2022-06-25 11:01:26 +0530

    .

--- Housing/entity/config_entity.py
+++ Housing/entity/config_entity.py
@@ -1,2 +1,7 @@
 from collections import namedtuple
 DataIngestionConfig = namedtuple("DataIngestionConfig",["dataset_download_url","tgz_download_dir","ingested_train_dir","ingested_test_dir"])
+DataValidationConfig = namedtuple("DataValidationConfig",["schema_file_path"])
+DataTransformationConfig = namedtuple("DataTransformationConfig",["add_bedroom_per_room",
+                                                                    "transfromed_train_dir",
+                                                                    "transfromed_test_dir",
+                                                                    "preprocessed_object_file_path"])
